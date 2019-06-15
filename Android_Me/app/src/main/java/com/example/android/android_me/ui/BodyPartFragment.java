11439f52ed28f8aa13146c3ccdfedb16e6128fae
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-06-15 22:39:37 +0200

    Set the image resources

--- Android_Me/app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
+++ Android_Me/app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -6,8 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,11 +22,23 @@ public class BodyPartFragment extends Fragment {
     }
 
 
+    /**
+     * Inflates the fragment layout file and sets the correct resource for the image to display
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_body_part, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
+
+        // Get a reference to the ImageView in the fragment layout
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
+
+        // Set the image to the first in our list of head images
+        imageView.setImageResource(AndroidImageAssets.getHeads().get(0));
+
+        // Return the rootView
+        return rootView;
     }
 
 }
