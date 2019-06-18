3e5b1330bfcad86e652902bfa23221445f9ca147
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-06-18 23:26:15 +0200

    Implement the interface

--- Android_Me/app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ Android_Me/app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.android.android_me.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.example.android.android_me.R;
 
-public class MainActivity extends AppCompatActivity {
+// This activity is responsible for displaying the master list of all images
+// Implement the MasterListFragment callback, OnImageClickListener
+public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // Define the behavior for onImageSelected
+    public void onImageSelected(int position) {
+        // Create a Toast that displays the position that was clicked
+        Toast.makeText(this, "Position clicked = " + position, Toast.LENGTH_SHORT).show();
+    }
+
 }
