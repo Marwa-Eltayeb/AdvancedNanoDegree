324677e43ae6452b805e48a580e13196d145bad0
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-06-30 23:15:50 +0200

    Test Runner Annotation

---
+++ TeaTime/app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
@@ -0,0 +1,19 @@
+package com.example.android.teatime;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+/**
+ * This test demos a user clicking on a GridView item in MenuActivity which opens up the
+ * corresponding OrderActivity.
+ *
+ * This test does not utilize Idling Resources yet. If idling is set in the MenuActivity,
+ * then this test will fail. See the IdlingResourcesTest for an identical test that
+ * takes into account Idling Resources.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class MenuActivityScreenTest {
+
+}
