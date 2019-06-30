e35e5f702f278cb940927031a632756b66214bdd
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-06-30 23:22:03 +0200

    Check OrderActivity opens with the correct tea name

--- TeaTime/app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
+++ TeaTime/app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
@@ -4,7 +4,16 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
 
 /**
  * This test demos a user clicking on a GridView item in MenuActivity which opens up the
@@ -30,4 +39,17 @@ public class MenuActivityScreenTest {
     @Rule
     public ActivityTestRule<MenuActivity> mActivityTestRule = new ActivityTestRule<>(MenuActivity.class);
 
+    /**
+     * Clicks on a GridView item and checks it opens up the OrderActivity with the correct details.
+     */
+    @Test
+    public void clickGridViewItem_OpensOrderActivity() {
+
+        // Uses {@link Espresso#onData(org.hamcrest.Matcher)} to get a reference to a specific
+        // gridview item and clicks it.
+        onData(anything()).inAdapterView(withId(R.id.tea_grid_view)).atPosition(1).perform(click());
+
+        // Checks that the OrderActivity opens with the correct tea name displayed
+        onView(withId(R.id.tea_name_text_view)).check(matches(withText(TEA_NAME)));
+    }
 }
