df27375080c76e6e1f0250ab2ef1c86c3ef95c16
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-06-30 22:12:46 +0200

    Test Runner Annotation

--- TeaTime/app/src/androidTest/java/com/example/android/teatime/OrderActivityBasicTest.java
+++ TeaTime/app/src/androidTest/java/com/example/android/teatime/OrderActivityBasicTest.java
@@ -1,27 +1,15 @@
-/*
-* Copyright (C) 2017 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*  	http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
 package com.example.android.teatime;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
 
 /**
  * This test demos a user clicking the decrement button and verifying that it properly decrease
  * the quantity the total cost.
  */
 
-// TODO (1) Add annotation to specify AndroidJUnitRunner class as the default test runner
+@RunWith(AndroidJUnit4.class)
 public class OrderActivityBasicTest {
 
     // TODO (2) Add the rule that provides functional testing of a single activity
