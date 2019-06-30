ac4e1000135b044c41028a3a8094f452138cac46
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-06-30 23:16:54 +0200

    Rule to test a single Activity

--- TeaTime/app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
+++ TeaTime/app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
@@ -1,13 +1,15 @@
 package com.example.android.teatime;
 
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.runner.RunWith;
 
 /**
  * This test demos a user clicking on a GridView item in MenuActivity which opens up the
  * corresponding OrderActivity.
- *
+ * <p>
  * This test does not utilize Idling Resources yet. If idling is set in the MenuActivity,
  * then this test will fail. See the IdlingResourcesTest for an identical test that
  * takes into account Idling Resources.
@@ -15,5 +17,17 @@ import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class MenuActivityScreenTest {
+
+    public static final String TEA_NAME = "Green Tea";
+
+    /**
+     * The ActivityTestRule is a rule provided by Android used for functional testing of a single
+     * activity. The activity that will be tested will be launched before each test that's annotated
+     * with @Test and before methods annotated with @Before. The activity will be terminated after
+     * the test and methods annotated with @After are complete. This rule allows you to directly
+     * access the activity during the test.
+     */
+    @Rule
+    public ActivityTestRule<MenuActivity> mActivityTestRule = new ActivityTestRule<>(MenuActivity.class);
 
 }
