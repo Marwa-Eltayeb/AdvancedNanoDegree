8460b4db86af0887089b33d71035627ad1e79a12
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-07-03 00:42:06 +0200

    Runner test annotation

---
+++ TeaTime/app/src/androidTest/java/com/example/android/teatime/IdlingResourceMenuActivityTest.java
@@ -0,0 +1,14 @@
+package com.example.android.teatime;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+/**
+ * Created by Marwa on 7/3/2019.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class IdlingResourceMenuActivityTest {
+
+}
