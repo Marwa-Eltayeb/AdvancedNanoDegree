674efd50a655b3a7cc4a15b9ab231cdc9ae549bd
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-07-03 00:45:57 +0200

    Test Tea objects appeared in the GridView

--- TeaTime/app/src/androidTest/java/com/example/android/teatime/IdlingResourceMenuActivityTest.java
+++ TeaTime/app/src/androidTest/java/com/example/android/teatime/IdlingResourceMenuActivityTest.java
@@ -7,7 +7,13 @@ import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.anything;
 
 /**
  * Created by Marwa on 7/3/2019.
@@ -40,4 +46,11 @@ public class IdlingResourceMenuActivityTest {
         Espresso.registerIdlingResources(mIdlingResource);
     }
 
+    @Test
+    public void idlingResourceTest() {
+        onData(anything()).inAdapterView(withId(R.id.tea_grid_view)).atPosition(0).perform(click());
+    }
+
+
+    
 }
