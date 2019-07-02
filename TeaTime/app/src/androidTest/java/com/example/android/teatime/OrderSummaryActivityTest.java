4a3bec1d9fe81984ae54b824dedc02dc1032c7dd
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-07-02 21:57:37 +0200

    Stub All External Intents

--- TeaTime/app/src/androidTest/java/com/example/android/teatime/OrderSummaryActivityTest.java
+++ TeaTime/app/src/androidTest/java/com/example/android/teatime/OrderSummaryActivityTest.java
@@ -1,10 +1,18 @@
 package com.example.android.teatime;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.isInternal;
+import static org.hamcrest.core.IsNot.not;
+
 
 @RunWith(AndroidJUnit4.class)
 public class OrderSummaryActivityTest {
@@ -23,6 +31,14 @@ public class OrderSummaryActivityTest {
     @Rule
     public IntentsTestRule<OrderSummaryActivity> mActivityRule = new IntentsTestRule<>(
             OrderSummaryActivity.class);
+
+
+    @Before
+    public void stubAllExternalIntents() {
+        // By default Espresso Intents does not stub any Intents. Stubbing needs to be setup before
+        // every test run. In this case all external Intents will be blocked.
+        intending(not(isInternal())).respondWith(new Instrumentation.ActivityResult(Activity.RESULT_OK, null));
+    }
 
 
 }
