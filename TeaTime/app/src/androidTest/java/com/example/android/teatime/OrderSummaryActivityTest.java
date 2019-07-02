26bf82f777cb40ec75a7a520583b376f89714bf0
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-07-02 21:55:48 +0200

    Rule for Espresso-intents APIs

--- TeaTime/app/src/androidTest/java/com/example/android/teatime/OrderSummaryActivityTest.java
+++ TeaTime/app/src/androidTest/java/com/example/android/teatime/OrderSummaryActivityTest.java
@@ -1,9 +1,28 @@
 package com.example.android.teatime;
 
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class OrderSummaryActivityTest {
+
+    private static final String emailMessage = "I just ordered a delicious tea from TeaTime. Next time you are craving a tea, check them out!";
+
+    /**
+     *
+     * This test demonstrates Espresso Intents using the IntentsTestRule, a class that extends
+     * ActivityTestRule. IntentsTestRule initializes Espresso-Intents before each test that is annotated
+     * with @Test and releases it once the test is complete. The designated Activity
+     * is also terminated after each test.
+     *
+     */
+
+    @Rule
+    public IntentsTestRule<OrderSummaryActivity> mActivityRule = new IntentsTestRule<>(
+            OrderSummaryActivity.class);
+
+
 }
